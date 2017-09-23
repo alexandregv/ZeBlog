d4be2a35996558a130df42da00acbe589357a011
TriiNoxYs <contact@triinoxys.fr>
2017-09-23 21:32:33 +0000

    Rename app to ZeBlog (+ session store name)

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Test
+module ZeBlog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     
     #config.autoload_paths << "#{Rails.root}/app/validators"
+    
+    config.site = {
+      name: 'ZeBlog'
+    }
     
     config.generators do |g|
       g.assets            false

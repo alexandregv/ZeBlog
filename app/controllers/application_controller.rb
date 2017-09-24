358d79f17fd38a5236d9b7fe11fb3dea7d894e40
TriiNoxYs <contact@triinoxys.fr>
2017-09-24 02:02:53 +0000

    Make navbar dynamic about users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :danger
+  helper_method :current_user
   
   
   private
+  
   def only_signed_in
-    if !session[:user] || !session[:user]['id']
+    if !current_user
         redirect_to new_user_path, danger: 'Vous devez vous connecter pour accéder à cette page.'
     end
+  end
+
+  def current_user 
+    return nil if !session[:user] || !session[:user]['id'] 
+    return User.find(session[:user]['id']) 
   end
   
 end

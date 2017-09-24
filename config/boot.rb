ff48706b7258b3a8e8924766560e0d9bd9879fdb
TriiNoxYs <contact@triinoxys.fr>
2017-09-24 13:48:38 +0000

    Set TMPDIR to /tmp

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['TMPDIR'] ||= File.expand_path('../tmp/', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
